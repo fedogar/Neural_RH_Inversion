829ae7b03cc1a7088eaaec07a6e8483d5261d52e
Ferran <fedogar@alumni.uv.es>
2020-04-02 14:58:47 +0200

    Modificaciones del 2 de Avril: Inicio de mes intensivo de TFG::

--- Data/Charge_Repo/Charge.py
+++ Data/Charge_Repo/Charge.py
@@ -40,12 +40,12 @@ class ChargerIN ():
             for j in np.arange(504):
                 for k in np.arange(504):
                     MacroT[k,j,i] = ttext[i+j+k]
-        MacroT = torch.from_numpy(MacroZ)            
+        MacroT = torch.from_numpy(MacroT)            
         for i in np.arange(161):
             for j in np.arange(504):
                 for k in np.arange(504):
                     MacroP[k,j,i] = ftext[i+j+k]
-        MacroP = torch.from_numpy(MacroZ) 
+        MacroP = torch.from_numpy(MacroP) 
         for i in np.arange(161):
             MacroZ[i] = ztext[i]
         MacroZ = torch.from_numpy(MacroZ)
@@ -94,4 +94,50 @@ class ChargerOUT():
         MacroTA =  torch.from_numpy(MacroTA)
         
         return [MacroTT,Macroe, MacroTA]
+    
+
+class LoadSintetic ():
+    
+    def __init__(self):
+    
+        self.files = ['./Charge_Repo/File_of_Variables/azimuth.txt, /Charge_Repo/File_of_Variables/field.txt, /Charge_Repo/File_of_Variables/incli.txt, /Charge_Repo/File_of_Variables/pgas.txt, /Charge_Repo/File_of_Variables/rho.txt, /Charge_Repo/File_of_Variables/vel.txt, /Charge_Repo/File_of_Variables/zeta.txt']
+        self.columns = self.LoadTable()
+            
+    def LoadTable (self):
+        
+        x = torch.randn(len(self.files),504,504,86)
+        count=0
+        
+        for i in self.files :
+            f = open (i)
+            f = f.read()
+            f = f.split()
+    
+            for s in np.arange(86):
+                for j in np.arange(504):
+                    for k in np.arange(504):
+                        x[i][k][j][s] = f[k+j+s]
+            count =count+1
+            
+        return x
+
+            
+            
+            
+"""      
+class Sintesis_Table (object):
+
+    def __init__(self, LoadModuleCL, LoadSintetic):
+    
+        self.Table_ascii = self.table_append(LoadModuel.testing, LoadSintetic.columns)
+        self.Location = LoadSintetic.files()
+        self.Load = 
+        self.
+
+    def table_append (self, LoadModuel.testing, LoadSintetic.columns):
+        
+        for i in LoadModuel.testing:
+"""          
+            
+        
         
--- Data/Processing_Repo/Processing.py
+++ Data/Processing_Repo/Processing.py
@@ -27,11 +27,9 @@ from Charge_Repo.Charge import ChargerIN
 from Charge_Repo.Charge import ChargerOUT
 
 
-class ModuleList():
+class ModuleList(object):
     def __init__(self):
         self.list = ['LinearNet', 'GANS', 'Convolutional']
-
-
 
 class GANS (nn.Module):
 
@@ -83,16 +81,16 @@ class GANS (nn.Module):
 class LinearNet(nn.Module):
    
     def __init__(self):
-        super().__init__()
+        super(LinearNet,self).__init__()
         self.fc1 = nn.Linear(161*3,200)
         self.fc2 = nn.Linear(200,100)
         self.fc3 = nn.Linear(100,50)
         self.fc4 = nn.Linear(50,86*3)
 
-    def forward(self,x):
-        x = F.relu(self.fc1(x))
-        x = F.leaky_relu(self.fc2(x))
-        x = F.leaky_relu(self.fc3(x))
+    def forward(self,x,y,z):
+        x = F.threshold(self.fc1(x),y,z)
+        x = F.threshold(self.fc2(x),y,z)
+        x = F.threshold(self.fc3(x),y,z)
         x = self.fc4(x)
         return x
 
@@ -102,126 +100,161 @@ class LinearNet(nn.Module):
         return x
 
 #Actually is passed linear net, but in main we can pass the model we want with correct match ChargeIN, ChargeOut 
-class LoadModule (ChargerIN,ChargerOUT, LinearNet):
+class LoadModule (object):
     
-    def __init__ (self):
-
+    def __init__ (self,ChargerIN,ChargerOUT,LinearNet):
+        super(LoadModule,self).__init__()
+        self.lr = 0.05
         self.training = ChargerIN().in_
         self.testing = ChargerOUT().out_
         self.module = LinearNet()
-        self.optimDict = list()
-        self.netDict = list()
+        self.netDict = {}
         self.losses = list()
 
-    def train(self,trainList):
+    def train (self, lr_):
 
         for i in np.arange(3):
             self.training[i] = self.training[i].view(-1,161)
             self.testing[i] = self.testing[i].view(-1,161)
 
         self.netDict.clear()
-        self.netDict.clear()
+        self.optimDict.clear()
 
         for epoch in np.arange(504*504):
 
             packTest = torch.cat((self.testing[0][epoch][:], self.testing[1][epoch][:],self.testing[2][0][:]),1)
             packTrain = torch.cat((self.training[0][epoch][:], self.testing[1][epoch][:],self.training[2][0][:]),1)
             
-            output = self.module.forward(packTrain)
+            output = self.module.forward(packTrain.float())
             self.module.zero_grad()
-            optimizer = optim.SGD(self.module.parameters(), lr=0.01)
+            optimizer = optim.SGD(self.module.parameters(), lr_)
             optimizer.zero_grad()
-            loss = nn.MSELoss(output,packTest)
-            self.losses.append(loss.backward())
+            loss =F.mse_loss(output,packTest)
+            if loss == float('Inf'):
+                break 
+            loss.backward()
+            self.losses.append(loss)
             optimizer.step()
-
-        self.optimDict.append(optimizer.state_dict)
-        self.netDict.append(self.module.state_dict)
-
-    def netState(self,trainList):
-        return trainList[:][1]
-
-    def optimState(self,trainList):
-        return trainList[:][0]
-
+            try:
+                self.netDict = { 'fc1' : self.fc1.weight, 'fc2' : self.fc2.weight, 'fc3' :self.fc3.weight , 'fc4' : self.fc4.weight } 
+            except NameError:
+                    print('Error')
 
 class Convolutional1DNet(nn.Module):
-    
-    #Esta codeado para una longitud de entrada de 161 a 86: sea 86 la longitud de salida del sistema.
-    def __init__():
-        super().__init__():
-        self.conv1 = nn.Conv1d(3,2,74)
-        self.conv1 = nn.Conv1d(2,3,86)
-    
-    def forward(x):
-        x = F.leaky_relu(self.conv1(x))
-        x = self.conv2(x)
-        return x
 
-class LoadModuleConv (ChargerIN,ChargerOUT,Convolutional1DNet ):
-    def __init__():
+    def __init__(self):
+        super().__init__()
+        
+        self.conv1 = nn.Conv1d(3,2,74)
+        self.conv2 = nn.Conv1d(2,3,8,padding=10 )
+        self.conv3 = nn.Conv1d(3,2,14)
+        self.conv4 = nn.Conv1d(2,3,3)
+        
+        
+    def forward(self,x):
+        
+        
+        x = F.tanh(self.conv1(x))
+        x = F.tanh(self.conv2(x))
+        x = F.tanh(self.conv3(x))
+        x = self.conv4(x)
+        
+        return x
+    
+#It needs to be passed this 3 modules in the terminal call one is load
+class LoadModuleCL (object):
+    def __init__(self,ChargerIN,ChargerOUT,Convolutional1DNet):
+        self.lr = 0.09
+        self.module2 = Convolutional1DNet()
         self.training = ChargerIN().in_
-        self.testing = ChargerOUT().in_
-        self.module = Convolutional1DNet()
-        self.oDictpxp = list()
-        self.oDtpack = list()
-        self.oNpxp = list()
-        self.oNpxpack = list()
+        self.testing = ChargerOUT().out_
+        self.nDpxp = {}
+        self.nDpack = {}
         self.losspack = list()
         self.losspxp = list()
-
-    def testing_PxP():
-        
+    def testing_PackxPack(self,lr_):
+        self.lr = lr_
         for i in np.arange(3):
             self.training[i] = self.training[i].view(-1,1,161)
-            self.testing[i] = self.testing[i].view(-1,1,161)
-            
-        for epoch in np.arange(504*504):
-            
-            #In this form take convolution pixel to pixel / does not allow correlations between pixels ::
-            packTest = torch.cat((self.testing[0][epoch][:], self.testing[1][epoch][:],self.testing[2][0][:]),1)
-            packTrain = torch.cat((self.training[0][epoch][:], self.testing[1][epoch][:],self.training[2][0][:]),1)
-        
-            output = self.module.forward(packTrain)
-            self.module.zero_grad()
-            optimizer = optim.SGD(self.module.parameters(), lr=0.01)
-            optimizer.zero_grad()
-            loss = nn.MSELoss(output,packTest)
-            self.losspxp.append(loss.backward())
-            optimizer.step()
-        
-        self.oDpxp.append(optimizer.state_dict)
-        self.nDpxp.append(self.module.state_dict)
-
-
-    def testing_PackxPack():
-
-        for i in np.arange(3):
-            self.training[i] = self.training[i].view(-1,1,161)
-            self.testing[i] = self.testing[i].view(-1,1,161)
-            
+            self.testing[i] = self.testing[i].view(-1,1,86)
         for epoch in np.arange(3969):
-            
             #In this form take convolution pixel to pixel / does not allow correlations between pixels ::
-            packTest = torch.cat((self.testing[0][epoch][64*epoch:64*(epoch+1)], self.testing[1][64*epoch:64*(epoch+1)][:],(self.testing[2][0][:].repeat(64,161)),1)
-            packTrain = torch.cat((self.training[0][epoch][64*epoch:64*(epoch+1)], self.testing[1][epoch][64*epoch:64*(epoch+1)],(self.testing[2][0][:].repeat(64,161)),1)
-        
-            output = self.module.forward(packTrain)
-            self.module.zero_grad()
-            optimizer = optim.SGD(self.module.parameters(), lr=0.01)
+            packTest = torch.cat((self.testing[0][64*epoch:64*(epoch+1)][:], self.testing[1][64*epoch:64*(epoch+1)][:],(self.testing[2]).repeat(64,1,1)),1)
+            packTrain = torch.cat((self.training[0][64*epoch:64*(epoch+1)][:], self.training[1][64*epoch:64*(epoch+1)][:],(self.training[2]).repeat(64,1,1)),1)
+            self.module2 = (self.module2).float()
+            output = (self.module2).forward(packTrain.float())
+            (self.module2).zero_grad()
+            optimizer = optim.SGD((self.module2).parameters(), lr_)
             optimizer.zero_grad()
-            loss = nn.MSELoss(output,packTest)
-            self.losspack.append(loss.backward())
+            loss = F.mse_loss(output,packTest.float())
+            if loss == float('Inf'):
+                break 
+            loss.backward()
+            (self.losspack).append(loss)
             optimizer.step()
+            if epoch == 3968:
+                self.nDpack = {}
+
+    def testing_PxP(self,lr_):
+        self.lr = lr_
+        for i in np.arange(3):
+            self.training[i] = self.training[i].view(-1,1,161)
+            self.testing[i] = self.testing[i].view(-1,1,86)
+        for epoch in np.arange(504*504):
+            #In this form take convolution pixel to pixel / does not allow correlations between pixels ::
+            packTest = torch.cat((self.testing[0][epoch].view(-1,1,86), self.testing[1][epoch].view(-1,1,86),self.testing[2]),1)
+            packTrain = torch.cat((self.training[0][epoch].view(-1,1,161), self.training[1][epoch].view(-1,1,161),self.training[2]),1)
+            self.module2 = (self.module2).float()
+            output = self.module2.forward(packTrain.float())
+            self.module2.zero_grad()
+            optimizer = optim.SGD(self.module2.parameters(), lr=lr_)
+            optimizer.zero_grad()
+            loss = F.mse_loss(output,packTest.float())
+            if loss == float('Inf'):
+                break 
+            loss.backward()
+            self.losspxp.append(loss)
+            optimizer.step()
+            if epoch == 254015:
+                try:
+                    self.nDpxp = { 'conv1' : self.module2.conv1.weight, 'conv2' : self.module2.conv2.weight, 'conv3' :self.module2.conv3.weight , 'conv4' : self.module2.conv1.weight } 
+                except NameError:
+                    self.nDpxp={ 'conv1' : self.module2.conv1.weight}
+                                      
+class LoadShuffle (object):
+    
+    def __init__(self,ChargerIN,ChargerOUT,Convolutional1DNet,ShuffleSplit):
+        self.training = ChargerIN().in_
+        self.testing = ChargerOUT().out_
+        self.shuffel = self.prepare()
+        self.randoomstate = 4
         
-        self.oDpack.append(optimizer.state_dict)
-        self.nDpack.append(self.module.state_dict)
+        def prepare(self):
+            for i in no.arange(3):
+                self.testing[i].view(-1,1,86)     
+                self.training[i](-1,1,161)
+            
+            pack = torch.cat((self.testing[0],self.testing[1],self.testing[2].repeat(self.testing[0].size()[0],1,1),self.training[0],self.training[1],self.training[2].repeat(self.training[0].size()[0],1,1)),1)
+            ss = ShuffleSplit(pack.size()[0], test_size=0.25, random_state=4)
+            
+            #Here try to shuffle the vector for the training          
+"""
+Here we define the classes related to the overfitting, underfitting checking for the different examples
+"""
+from sklearn.model_selection import ShuffleSplit
 
+def ValidationTest (object):
+    
+    ss = ShuffleSplit(n_splits=2540156, test_size=0.25, random_state=0)
+    cl = LoadModuleCL(ChargerIN,ChargerOUT,Convolutional1DNet,LinearNet)
+    for i in no.arange(3):
+        cl.testing[i].view(-1,1,86)     
+        cl.training[i](-1,1,161)
+    
+    
+    
+    
 
-
-
-
-                
 
 
             
